0bdcddaffda26bc6bba4c69736926a979c5492fe
hanasuey <ouaoudahana@gmail.com>
2020-06-03 19:37:06 +0200

    database3 taak 2

--- 3-Delete/taak02/antwoorden.sql
+++ 3-Delete/taak02/antwoorden.sql
@@ -1,5 +1,13 @@
 -- Opdracht 3
 
+SELECT * FROM players WHERE id = "198329"
+
+id    name    age    nationality    club    value    wage
+198329    Rodrigo     26    Spain     Valencia CF     13000000    36000
+198329    Rodrigo     26    Spain     Valencia CF     13000000    36000
+
 -- Opdracht 4
+DELETE FROM players WHERE id = "198329"
 
 -- Opdracht 5
+ Your SQL query has been executed successfully.
