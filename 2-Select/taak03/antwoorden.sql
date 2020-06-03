7449a5259ed03fd880112ca23f7cb88e6bb726e6
hanasuey <ouaoudahana@gmail.com>
2020-06-03 17:03:47 +0200

    database2 taak 3

--- 2-Select/taak03/antwoorden.sql
+++ 2-Select/taak03/antwoorden.sql
@@ -1,12 +1,80 @@
 -- Opdracht 1 
 
--- Opdracht 2 
+SELECT * FROM players WHERE club = "Chelsea" AND nationality = "Spain"
+
+
+id	name	age	nationality	club	value	wage	
+162895	Cesc F bregas 	30	Spain 	Chelsea 	41000000	210000	
+179844	Diego Costa 	28	Spain 	Chelsea 	46000000	235000	
+184432	Azpilicueta 	27	Spain 	Chelsea 	375000000	160000	
+189505	Pedro 	29	Spain 	Chelsea 	305000000	200000	
+192638	Marcos Alonso 	26	Spain 	Chelsea 	175000000	115000	
+201153	Morata 	24	Spain 	Chelsea 	41000000	170000	
+
+
+-- Opdracht 2
+
+SELECT * FROM players WHERE age = "17" AND nationality = "Spain" AND wage = "15000"
+
+
+
+231410	Brahim D az 	17	Spain 	Manchester City 	13000000	15000	
+
 
 -- Opdracht 3
 
+SELECT * FROM players WHERE club = "Liverpool" AND age = "20"
+
+
+id	name	age	nationality	club	value	wage	
+220710	H. Wilson 	20	Wales 	Liverpool 	550000	13000	
+225100	J. Gomez 	20	England 	Liverpool 	28000000	40000	
+
+
 -- Opdracht 4
 
+SELECT * FROM players WHERE nationality = "Netherlands" AND club = "Ajax"
+
+
+id	name	age	nationality	club	value	wage	
+148803	K. Huntelaar 	33	Netherlands 	Ajax 	55000000	17000	
+186452	S. de Jong 	28	Netherlands 	Ajax 	75000000	16000	
+193440	N. Viergever 	27	Netherlands 	Ajax 	65000000	14000	
+204367	B. van Leer 	25	Netherlands 	Ajax 	7000000	11000	
+208004	J. Veltman 	25	Netherlands 	Ajax 	85000000	13000	
+208459	M. Dijks 	24	Netherlands 	Ajax 	55000000	10000	
+216380	D. Sinkgraven 	21	Netherlands 	Ajax 	75000000	9000	
+221283	A. Nouri 	20	Netherlands 	Ajax 	75000000	10000	
+221363	D. van de Beek 	20	Netherlands 	Ajax 	115000000	12000	
+223811	N. Alblas 	22	Netherlands 	Ajax 	675000	2000	
+228702	F. de Jong 	20	Netherlands 	Ajax 	5000000	7000	
+234198	D. Dankerlui 	20	Netherlands 	Ajax 	500000	2000	
+234203	C. Eiting 	19	Netherlands 	Ajax 	950000	2000	
+235243	M. de Ligt 	17	Netherlands 	Ajax 	115000000	7000	
+235899	D. Zeefuik 	19	Netherlands 	Ajax 	725000	2000	
+236920	J. Kluivert 	18	Netherlands 	Ajax 	7000000	8000	
+
+
 -- Opdracht 5
+
+SELECT name FROM players WHERE club = "Ajax" AND nationality != "Netherlands"
+
+
+name	
+L. Sch ne 	
+K. Lamprou 	
+A. Younes 	
+H. Ziyech 	
+V. ?ern  	
+L. Orejuela 	
+A. Onana 	
+A. Onana 	
+M. Cassierra 	
+K. Dolberg 	
+M. W ber 	
+David Neres 	
+D. Johnsen 	
+
 
 -- Opdracht 6 
 
